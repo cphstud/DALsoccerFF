992bb554764626f15b043bbac9d1b642b0193adb
thor <thor@cphbusiness.dk>
2023-03-03 08:25:37 +0100

    testing shot and ff with plots

--- main.R
+++ main.R
@@ -1,7 +1,10 @@
 source("util.R")
+source("mathUtil.R")
 
 gamedfmf<-get_shots(19748)
 ts=clean_shots(gamedfmf)
+
+
 
 #PLOT ALL SHOTS
 ggplot(ts) +
@@ -24,3 +27,38 @@ ggplot(ts) +
           "ggsoccer example")
 
 #PLOT ONE SHOT AND FREEZEFRAME
+# prep data - one shot + shot_ff
+gsFF= single_shot_ff(gamedfmf[4,])
+gsSh= single_shot(gamedfmf[4,])
+gsFF= single_shot_ff(gamedfmf[1,])
+gsSh= single_shot(gamedfmf[1,])
+gsFF= single_shot_ff(gamedfmf[9,])
+gsSh= single_shot(gamedfmf[9,])
+gsFF= single_shot_ff(gamedfmf[12,])
+gsSh= single_shot(gamedfmf[12,])
+gsFF= single_shot_ff(gamedfmf[22,])
+gsSh= single_shot(gamedfmf[22,])
+gsFF= single_shot_ff(gamedfmf[24,])
+gsSh= single_shot(gamedfmf[24,])
+
+
+ggplot(gsSh) +
+  annotate_pitch(
+    colour = "black",
+    fill   = "white",
+    limits = FALSE,
+    dimensions = pitch_statsbomb
+  ) +
+  geom_point(aes(x=x,y=y),size=3)+
+  geom_point(data=gsFF,aes(x=x,y=y, color=teammate),size=3)+
+  geom_segment(aes(x = x, y = y, xend = x2, yend = y2),
+               arrow = arrow(length = unit(0.15, "cm"),
+                             type = "closed")) +
+  theme_pitch() +
+  theme(panel.background = element_rect(fill = "springgreen4")) +
+  coord_flip(xlim = c(49, 120)) +
+  scale_y_reverse() +
+  direction_label() +
+  ggtitle("Simple passmap", 
+          "ggsoccer example")
+
