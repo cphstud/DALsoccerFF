f7ef46f27b0bb73996ef3632d2f3a385a3ae4a0b
thor <thor@cphbusiness.dk>
2023-03-03 08:24:55 +0100

    adding single shot and single shot ff

--- util.R
+++ util.R
@@ -33,3 +33,38 @@ clean_shots<-function(df){
   #colnames(ts2) <- c('team','x','y','x2','y2')}
   return(ts)
 }
+
+single_shot_ff<-function(df){
+  df <- df %>% select(shot) %>% unnest_wider(shot)
+  ff=df$freeze_frame[[1]] %>% as.data.frame() 
+  ff <- ff %>% unnest_wider(player)
+  ff <- ff %>% unnest_wider(position, names_sep = "_")
+  ff <- ff %>% unnest_wider(location, names_sep = "")
+  
+  ff <- ff %>% select(-c("id","position_id"))
+  names<-c("x","y","name","position_name","teammate")
+  colnames(ff)=names
+  
+  #ts2<-ts[,c(1,3:6)]
+  #colnames(ts2) <- c('team','x','y','x2','y2')}
+  return(ff)
+}
+
+single_shot<-function(df){
+  df <- df %>% unnest_wider(type, names_sep = "_")
+  df <- df %>% unnest_wider(possession_team, names_sep = "_")
+  df <- df %>% unnest_wider(team, names_sep = "_")
+  df <- df %>% unnest_wider(shot, names_sep = "_")
+  df <- df %>% unnest_wider(player, names_sep = "_")
+  df <- df %>% unnest_wider(location, names_sep = "")
+  df <- df %>% unnest_wider(shot_end_location, names_sep = "")
+  df <- df %>% unnest_wider(shot_end_location1, names_sep = "")
+  df <- df %>% unnest_wider(shot_outcome, names_sep = "_")
+  
+  ts<- df %>% select(player_name,team_name,shot_outcome_name,
+                     location1,location2,
+                     shot_end_location11,shot_end_location12)
+  names<-c("player","team","shotoutcome","x","y","x2","y2")
+  colnames(ts)=names
+  return(ts)
+}
